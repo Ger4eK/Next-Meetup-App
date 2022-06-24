32d077c4ac5b20b56997f2c53b38786c81fcfe54
Ger4eK <ger4ek228@gmail.com>
2022-06-24 12:54:13 +0300

    Link

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,19 @@
 //! our-domain.com/
 
+import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <h1>The Home Page</h1>
-  )
-}
+    <>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/news'>News</Link>
+        </li>
+        <li>Aboba</li>
+      </ul>
+    </>
+  );
+};
 
-export default HomePage  
+export default HomePage;
