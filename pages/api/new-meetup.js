60edbb10b5b1b472566c263201196563ab5184d9
Ger4eK <ger4ek228@gmail.com>
2022-06-25 16:14:42 +0300

    working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,23 @@
 //! /api/new-meetup
 
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://Oleh:1234@cluster0.jowobcl.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
 
-    
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({message: 'Meetup inserted!'})
   }
 };
 
