e7b4042eed0a1e2c31a2476749a64de2691ed22c
Ger4eK <ger4ek228@gmail.com>
2022-06-26 13:33:33 +0300

    sending http requests to our API route

--- pages/new-meetup.jsx
+++ pages/new-meetup.jsx
@@ -1,10 +1,23 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../components/meetups/NewMeetupForm/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    router.push('/');
   };
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
-export default NewMeetupPage
+export default NewMeetupPage;
